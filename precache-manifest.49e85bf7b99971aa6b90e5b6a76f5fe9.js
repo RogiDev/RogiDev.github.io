339443560917772d25fcefddd357f75309dcb4c2
RogiDev <31067797+RogiDev@users.noreply.github.com>
2019-01-12 17:39:47 +0200

    updated

---
+++ precache-manifest.49e85bf7b99971aa6b90e5b6a76f5fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d570074cba522999eb3a",
+    "url": "/movie-searcher/static/js/runtime~main.d570074c.js"
+  },
+  {
+    "revision": "acdc9de4346a8f0d4494",
+    "url": "/movie-searcher/static/js/main.acdc9de4.chunk.js"
+  },
+  {
+    "revision": "1c25afbc84a01a567f42",
+    "url": "/movie-searcher/static/js/2.1c25afbc.chunk.js"
+  },
+  {
+    "revision": "acdc9de4346a8f0d4494",
+    "url": "/movie-searcher/static/css/main.aa5b291f.chunk.css"
+  },
+  {
+    "revision": "408779bcfdf3be7a360c3d30275a0826",
+    "url": "/movie-searcher/index.html"
+  }
+];
