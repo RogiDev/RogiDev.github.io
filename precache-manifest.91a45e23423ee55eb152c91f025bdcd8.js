411c7d8a51a03ffe111fe7878ada09001ae5a576
RogiDev <31067797+RogiDev@users.noreply.github.com>
2019-01-12 06:47:19 +0200

    new!

---
+++ precache-manifest.91a45e23423ee55eb152c91f025bdcd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b980fe5f8305129700f",
+    "url": "/movie-searcher/index.html/static/js/runtime~main.9b980fe5.js"
+  },
+  {
+    "revision": "83264cda17d2a291c9fe",
+    "url": "/movie-searcher/index.html/static/js/main.83264cda.chunk.js"
+  },
+  {
+    "revision": "bcea00ada3d968853b7c",
+    "url": "/movie-searcher/index.html/static/js/1.bcea00ad.chunk.js"
+  },
+  {
+    "revision": "83264cda17d2a291c9fe",
+    "url": "/movie-searcher/index.html/static/css/main.186af8c3.chunk.css"
+  },
+  {
+    "revision": "900cf70d30f7ef72c829c385d65c75b9",
+    "url": "/movie-searcher/index.html/index.html"
+  }
+];
