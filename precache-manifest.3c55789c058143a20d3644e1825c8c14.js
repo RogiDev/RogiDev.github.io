54ec12c4a76cab5c5f64922f8fa3820a4329023f
RogiDev <31067797+RogiDev@users.noreply.github.com>
2019-01-12 06:22:19 +0200

    better site

---
+++ precache-manifest.3c55789c058143a20d3644e1825c8c14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aebf8812828bfc4c1eca",
+    "url": "/movie-searcher/static/js/runtime~main.aebf8812.js"
+  },
+  {
+    "revision": "a2485cd0c94785593938",
+    "url": "/movie-searcher/static/js/main.a2485cd0.chunk.js"
+  },
+  {
+    "revision": "bcea00ada3d968853b7c",
+    "url": "/movie-searcher/static/js/1.bcea00ad.chunk.js"
+  },
+  {
+    "revision": "a2485cd0c94785593938",
+    "url": "/movie-searcher/static/css/main.186af8c3.chunk.css"
+  },
+  {
+    "revision": "7d17d2dbf967494bdbfff83015e82bc7",
+    "url": "/movie-searcher/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6890ffa355fc02810750d3067988cb1.js"
+  "/movie-searcher/precache-manifest.3c55789c058143a20d3644e1825c8c14.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/movie-searcher/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
